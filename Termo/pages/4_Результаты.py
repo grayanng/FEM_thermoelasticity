d6c9eac7528d1fb35dcc7c44f8ef7317206be47a
nomadpd <ponomarenko.ds16@physics.msu.ru>
2023-03-23 09:22:56 +0300

    v0.2 (2d) change: fixerror with temperature (C to K) add graphics change graphs in streamlit

--- Termo/pages/4_Результаты.py
+++ Termo/pages/4_Результаты.py
@@ -3,8 +3,9 @@ from PIL import Image
 import os.path
 
 menu = st.sidebar.radio('***',
-    ('Крупная сетка 20х4 (mshr)',
-     'Сетка 50х10 (mshr)',
+    ('Сетка 20х4',
+     'Сетка 50х10',
+     'Сетка 50х50',
      'Сетка (gmsh)',)
 )
 
@@ -14,9 +15,9 @@ menu = st.sidebar.radio('***',
 # Результат численного моделирования
 
 """
-if menu == "Крупная сетка 20х4 (mshr)":
+if menu == "Сетка 20х4":
 
-    block1 = '''mesh = RectangleMesh(Point(0.0, 0.0), Point(0.02, 0.012), 20, 4)'''
+    block1 = '''mesh = RectangleMesh(Point(0.0, 0.0), Point(0.02, 0.012), 20, 4, 'left/right')'''
 
     st.code(block1,language='python')
     path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/mesh_mshr.png')
@@ -27,7 +28,7 @@ if menu == "Крупная сетка 20х4 (mshr)":
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение температуры')
 
-    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/ums.png')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/vms.png')
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение напряжений')
 
@@ -39,8 +40,12 @@ if menu == "Крупная сетка 20х4 (mshr)":
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение перемещений по y')
 
-if menu == "Сетка 50х10 (mshr)":
-    block1 = '''mesh = RectangleMesh(Point(0.0, 0.0), Point(0.02, 0.012), 50, 10)'''
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/u(m).png')
+    image = Image.open(path_to_image)
+    st.image (image, caption='Распределение перемещений')
+
+if menu == "Сетка 50х10":
+    block1 = '''mesh = RectangleMesh(Point(0.0, 0.0), Point(0.02, 0.012), 50, 10, 'left/right')'''
 
     st.code(block1, language='python')
     path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/mesh_mshr.png')
@@ -62,6 +67,38 @@ if menu == "Сетка 50х10 (mshr)":
     path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/u(y).png')
     image = Image.open(path_to_image)
     st.image(image, caption='Распределение перемещений по y')
+
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/u(m).png')
+    image = Image.open(path_to_image)
+    st.image (image, caption='Распределение перемещений')
+
+if menu == "Сетка 50х50":
+    block1 = '''mesh = RectangleMesh(Point(0.0, 0.0), Point(0.02, 0.012), 50, 50, 'left/right')'''
+
+    st.code(block1, language='python')
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v4/mesh_mshr.png')
+    image = Image.open(path_to_image)
+    st.image(image)
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v4/temp.png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение температуры')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v4/vms.png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение напряжений')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v4/u(x).png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение перемещений по x')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v4/u(y).png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение перемещений по y')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v4/u(m).png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение перемещений')
 
 if menu == "Сетка (gmsh)":
 
