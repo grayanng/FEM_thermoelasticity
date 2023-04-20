5dfe8730dd8be5692445cb89e490ba1f6a0aae65
ponomdi <ponomarenko,ds16@physics,msu,ru>
2023-04-20 14:42:51 +0300

    add graphics for second mesh

--- Termo_2/pages/6_Результаты.py
+++ Termo_2/pages/6_Результаты.py
@@ -26,6 +26,10 @@ T0 = Constant(40.)
 '''
 st.code(code, language='python')
 
+menu = st.sidebar.radio('***',
+    ('Сетка 1',
+     'Сетка 2')
+)
 
 
 
@@ -33,25 +37,48 @@ st.code(code, language='python')
 
 
 
+if menu == "Сетка 1":
 
-"""
-# Результат численного моделирования
+    """
+    # Результат численного моделирования
+    
+    """
 
-"""
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/tem.png')
+    image = Image.open(path_to_image)
+    st.image (image, caption='Распределение температуры')
 
-path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/tem.png')
-image = Image.open(path_to_image)
-st.image (image, caption='Распределение температуры')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/vms.png')
+    image = Image.open(path_to_image)
+    st.image (image, caption='Распределение напряжений')
 
-path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/vms1.png')
-image = Image.open(path_to_image)
-st.image (image, caption='Распределение напряжений')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/ux.png')
+    image = Image.open(path_to_image)
+    st.image (image, caption='Распределение перемещений по x')
 
-path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/ux.png')
-image = Image.open(path_to_image)
-st.image (image, caption='Распределение перемещений по x')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/uy.png')
+    image = Image.open(path_to_image)
+    st.image (image, caption='Распределение перемещений по y')
 
-path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/uy.png')
-image = Image.open(path_to_image)
-st.image (image, caption='Распределение перемещений по y')
+if menu == "Сетка 2":
+    """
+    # Результат численного моделирования
+
+    """
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/tem.png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение температуры')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/vms.png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение напряжений')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/ux.png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение перемещений по x')
+
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/uy.png')
+    image = Image.open(path_to_image)
+    st.image(image, caption='Распределение перемещений по y')
 
