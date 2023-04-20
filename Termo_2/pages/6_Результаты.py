a405a0cf7e03bbd2a1d2e05c2cdbe1b8ea94d9ac
ponomdi <ponomarenko,ds16@physics,msu,ru>
2023-04-20 15:41:06 +0300

    fix

--- Termo_2/pages/6_Результаты.py
+++ Termo_2/pages/6_Результаты.py
@@ -44,19 +44,19 @@ if menu == "Сетка 1":
     
     """
 
-    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/tem.png')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/tem2.png')
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение температуры')
 
-    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/vms.png')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/vms2.png')
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение напряжений')
 
-    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/ux.png')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/ux2.png')
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение перемещений по x')
 
-    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/uy.png')
+    path_to_image= os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v1/uy2.png')
     image = Image.open(path_to_image)
     st.image (image, caption='Распределение перемещений по y')
 
@@ -66,19 +66,19 @@ if menu == "Сетка 2":
 
     """
 
-    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/tem.png')
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/tem2.png')
     image = Image.open(path_to_image)
     st.image(image, caption='Распределение температуры')
 
-    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/vms.png')
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/vms2.png')
     image = Image.open(path_to_image)
     st.image(image, caption='Распределение напряжений')
 
-    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/ux.png')
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/ux2.png')
     image = Image.open(path_to_image)
     st.image(image, caption='Распределение перемещений по x')
 
-    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/uy.png')
+    path_to_image = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'images', './v2/uy2.png')
     image = Image.open(path_to_image)
     st.image(image, caption='Распределение перемещений по y')
 
